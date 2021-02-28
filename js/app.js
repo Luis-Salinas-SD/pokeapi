e11ad720ad7c3d0fb73465406da3d0502928c5de
Luis-Salinas-SD <paus.impala21@gmail.com>
2021-02-27 19:31:13 -0600

    Inicialización del repo

---
+++ js/app.js
@@ -0,0 +1,55 @@
+//$     Retorna un numero aleatorio del 1 al 150.
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+//% utilizacion de addeventlistener
+document.addEventListener('DOMContentLoaded', () => {
+    const random = getRandomInt(1, 150)
+    fetchData(random)
+})
+
+//*$ Función asincrona.
+const fetchData = async (id) => {
+    try {
+        const rsult = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        const data = await rsult.json() //en data se almacena todo el objeto de la API
+        console.log(data);
+        const pokemon = {
+            img: data.sprites.other.dream_world.front_default,
+            nombre: data.name,
+            hp: data.stats[0].base_stat,
+            tipo: data.types[0].type.name,
+            exp: data.base_experience,
+            ataque: data.stats[1].base_stat,
+            espaecial: data.stats[2].base_stat,
+            defensa: data.stats[3].base_stat
+        }
+
+        pintarCard(pokemon)
+
+    } catch (error) {
+        console.log("error 505");
+    }
+}//$ fetchData fin.
+
+
+const pintarCard = (pokemon) => {
+    console.log(pokemon);
+    const flex = document.querySelector('.flex')
+    const template = document.querySelector('#template-card').content
+    const clone = template.cloneNode(true)
+    const fragment = document.createDocumentFragment()
+
+    clone.querySelector('.card-body-img').setAttribute('src', pokemon.img)
+    clone.querySelector('.card-body-title').innerHTML = `${pokemon.nombre} <span>${pokemon.hp} hp</span>`
+    clone.querySelector('.card-body-text').textContent = `Tipo: ` + pokemon.tipo
+    clone.querySelectorAll('.card-footer-social h3')[0].textContent = pokemon.ataque
+    clone.querySelectorAll('.card-footer-social h3')[1].textContent = pokemon.espaecial
+    clone.querySelectorAll('.card-footer-social h3')[2].textContent = pokemon.defensa
+
+
+
+    fragment.appendChild(clone)
+    flex.appendChild(fragment)
+}
